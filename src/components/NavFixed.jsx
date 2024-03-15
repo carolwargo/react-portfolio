154f1b26f01ca72c481ef3b29b469817b36471b5
Carol Wargo <Carolwargo@gmail.com>
2024-03-14 23:09:11 -0400

    updatte

--- src/components/NavFixed.jsx
+++ src/components/NavFixed.jsx
@@ -1,27 +1,28 @@
+
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function BasicExample() {
   return (
     <div>
+
       <Navbar expand="lg" className="bg-body-tertiary shadow-sm fixed-top">
         <Container fluid>
           <Navbar.Brand href="/"><b>CW</b>HOME</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-            <Nav.Link href="/">ABOUT</Nav.Link>
+              <Nav.Link href="/">ABOUT</Nav.Link>
               <Nav.Link href="/">RESUME</Nav.Link>
               <Nav.Link href="#link">PORTFOLIO</Nav.Link>
               <Nav.Link href="/">CONTACT</Nav.Link>
-              
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,100 +1,52 @@
 import React from "react";
-import HomeBig from "../assets/images/Home/HomeBig.png";
+import MeWhite from "../assets/images/MeWhite.png";
+import ResumeDownload from "../assets/ResumeDownload.png";
 
-
-const Header = () => {
+function Header() {
   return (
-    <header style={{ fontFamily: "Raleway" }}>
-      <div
-        id="intro-example"
-        className="p-4 bg-image position-relative"
-        style={{
-          backgroundImage: `url(${HomeBig})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundAttachment: "fixed",
-        }}
-      >
-        {/* Overlay div for masking */}
-        <div
-          className="position-absolute top-0 start-0 w-100 h-100"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-        ></div>
-
-        <div className="row d-flex h-100 px-3 text-center position-relative">
-          <div className="col-sm-12 col-md-3 col-lg-3"></div>
-          <div className="col-sm-12 col-md-6 col-lg-6 text-white p-3 position-relative z-index-1">
-            <h1
-              style={{
-                fontSize: "3.5rem",
-                fontFamily: "Raleway",
-              }}
-            >
-             WELCOME!
-            </h1>
-
-            <div className="p-2">
+    <div>
+      <header>
+        <div className="row w3-justify w3-center w3-margin-top w3-padding-24">
+          <div className="col-lg-5 ">
+          <img
+              src={MeWhite}
+              className="w3-image w3-round"
+              style={{ display: "block", width: "100%" }}
+              alt="Profile"
+            />
+          </div>
+          <div className="col-lg-6">
+            <div
+                 className="w3-container w3-center justify-content-center align-items-center"
+                 style={{ padding: "100px 16px" }}
+               >
+                 <h1 className="w3-jumbo">
+                   <b>Carol Wargo</b>
+                 </h1>
+                 <p>Full Stack Developer | UX Designer.</p>
             
-              <a href="https://carolwargo.github.io/me/">
-                {" "}
-                <button
-                  href="https://carolwargo.github.io/me/"
-                  type="button"
-                  className="btn btn-light p-5 w3-hover-opacity"
-                  style={{ color: "black", boxShadow: "0px 0px 14px #00ffff", textShadow: "1px 1px 2px black"}}
-                  data-mdb-ripple-init
-                ><span className="text-black" style={{fontSize: "2rem", textShadow: "0px 0px 1px white"}}>
-                  <b>CLICK ME TO NAVIGATE TO...</b>
-                </span>
-                <br />
-                  <b style={{ fontSize: "5rem", textShadow: "0px 0px 10px #00FFFF" }} className="">
-                    NEW SITE
-                  </b>
-                  <br />
-                  <span className="text-black" style={{ textShadow: "0px 0px 1px white"}}>
-              <b>THANKS IN ADVANCE- CAROL WARGO . </b>
-             </span>
-                </button>
-              </a>
-              <br />
-       
-
+                 <button className="w3-button w3-light-grey w3-padding-large w3-margin-top">
+                   <a
+                     className="button text-black"
+                     id="resume-download"
+                     download
+                     href={ResumeDownload}
+                   >
+                     <i className="fa fa-download"></i> Download Resume
+                   </a>
+                 </button>
+                 <br />
+                 <br />
+                 <br />
+                 <br />
+                 <a href="/about" className="w3-margin-bottom"><i className="fa fa-arrow-circle-right w3-xxxlarge" ></i></a>
+           <p className="text-center">NEXT</p>
             </div>
           </div>
-          <div className="col-sm-12 col-md-3 col-lg-3"></div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
-};
+}
 
 export default Header;
-
-//import React from "react";
-//import HomeIcons from "../components/Home/HomeIcons/HomeIcons.jsx";
-//import Header from "../components/Header/Header.jsx";
-//import HomeAbout from "../components/Home/HomeAbout/HomeAbout.jsx";
-//import '../App.css'
-//import "bootstrap/dist/css/bootstrap.min.css";
-
-/*
-    <div className="home" id="home" >
-      <div className="w3-center w3-padding-64" style={{fontFamily:'Raleway'}}>
-     
-
-     <div className="w3-padding-64">
-      <h2 className="er">VISIT MY NEW SITE...</h2>
-    <a href="https://carolwargo.github.io/me/"> <button href="https://carolwargo.github.io/me/" 
-     type="button" 
-     className="btn btn-info p-5 w3-hover-opacity shadow" style={{color:'white'}}data-mdb-ripple-init>
-      <b style={{fontSize:'5rem'}} className="">NEW SITE</b>
-      </button>
-      </a>
-      <br />
-      <br />
-     
-      <h3 className=''>THANKS IN ADVANCE- <br /><b>CAROL WARGO . </b></h3>
-     </div>
-   <HomeAbout/>
-        <HomeIcons />
-        */
