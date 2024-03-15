12a42364413f7b5b6b0f1f6978e7913618c9fc8b
Carol Wargo <Carolwargo@gmail.com>
2024-03-15 13:06:32 -0400

    update

--- src/components/NavFixed.jsx
+++ src/components/NavFixed.jsx
@@ -9,11 +9,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css"; // Import the CSS file where you define custom styles
 
 function BasicExample() {
+  console.log("Rendering BasicExample component"); // Add console.log statement here
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary shadow-sm fixed-top">
         <Container fluid>
-        <Link to="/" className="navbar-brand"><b>CW</b>HOME</Link>
+          <Link to="/" className="navbar-brand"><b>CW</b>HOME</Link>
           {/* Use Font Awesome icon instead of the default toggle icon */}
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <FontAwesomeIcon icon={faBars} style={{ color: "black" }} />
