bd8d967d0c6f7f39d295a3373922f7cd2b68ee43
Carol Wargo <Carolwargo@gmail.com>
2024-04-04 17:30:15 -0400

    update

--- src/components/Portfolio/Forms/Login.jsx
+++ src/components/Portfolio/Forms/Login.jsx
@@ -13,14 +13,13 @@ function Login() {
         e.preventDefault();
         console.log('Form login submitted');
 
- // Reset previous error messages
- setUsernameError('');
- setPasswordError('');
+        // Reset previous error messages
+        setUsernameError('');
+        setPasswordError('');
 
- // Reset form fields
- setUsername(''); // Reset username to empty string
- setPassword(''); // Reset password to empty string
-
+        // Reset form fields
+        setUsername(''); // Reset username to empty string
+        setPassword(''); // Reset password to empty string
 
         // Validation logic
         let isValid = true;
@@ -42,64 +41,68 @@ function Login() {
     };
 
     return (
-        <div className="login" id='login'>
-            <div className="w3-container w3-content bg-white shadow">
-                <div className="w3-padding-16 border-dark">
-                    <h4 className="fw-light w3-margin-top"><b>Login Form</b></h4>
-                    <div className="w3-padding-24">
-                        <form onSubmit={handleLogin}> {/* Wrap input fields with a form */}
+        <div className="row justify-content-center">
+            <div className="col-md-6 mx-auto">
+                <div className="login" id='login'>
+                    <div className="w3-container w3-content bg-white shadow">
+                        <div className="w3-padding-16 border-dark px-2">
+                            <h4 className="fw-light w3-margin-top"><b>Login Form</b></h4>
+                            <div className="w3-padding-24">
+                                <form onSubmit={handleLogin}> {/* Wrap input fields with a form */}
 
-                            <div className="form-outline mb-4">
-                            <input 
-    type="text" 
-    id="username" 
-    className={`form-control shadow-sm border border-1 ${usernameError ? 'is-invalid' : ''}`}
-    placeholder="Username"
-    value={username || 'Username'} // Use username state variable if not empty, otherwise use 'Username' placeholder
-    onChange={(e) => setUsername(e.target.value)}
-    autoComplete="username"
-/>
-                                <div className="invalid-feedback">{usernameError}</div>
-                            </div>
-
-                            <div className="form-outline mb-4">
-                            <input 
-    type="password" 
-    id="new-password" 
-    className={`form-control shadow-sm border border-1 ${passwordError ? 'is-invalid' : ''}`}
-    placeholder="Password"
-    value={password || 'Password'} // Use password state variable if not empty, otherwise use 'Password' placeholder
-    onChange={(e) => setPassword(e.target.value)}
-    autoComplete="new-password"
-/>
-                                <div className="invalid-feedback">{passwordError}</div>
-                            </div>
-
-                            <div className="row mb-4">
-                                <div className="col d-flex">
-                                    <div className="form-check">
+                                    <div className="form-outline mb-4">
                                         <input 
-                                            type="checkbox" 
-                                            id="rememberMe" 
-                                            className="form-check-input" 
+                                            type="text" 
+                                            id="username" 
+                                            className={`form-control shadow-sm border border-1 ${usernameError ? 'is-invalid' : ''}`}
+                                            placeholder="Username"
+                                            value={username || 'Username'} // Use username state variable if not empty, otherwise use 'Username' placeholder
+                                            onChange={(e) => setUsername(e.target.value)}
+                                            autoComplete="username"
                                         />
-                                        <label className="form-check-label" htmlFor="rememberMe"> Remember me </label>
+                                        <div className="invalid-feedback">{usernameError}</div>
                                     </div>
-                                </div>
-                                <div className="col">
-                                    <a href="#!">Forgot password?</a>
-                                </div>
+
+                                    <div className="form-outline mb-4">
+                                        <input 
+                                            type="password" 
+                                            id="new-password" 
+                                            className={`form-control shadow-sm border border-1 ${passwordError ? 'is-invalid' : ''}`}
+                                            placeholder="Password"
+                                            value={password || 'Password'} // Use password state variable if not empty, otherwise use 'Password' placeholder
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            autoComplete="new-password"
+                                        />
+                                        <div className="invalid-feedback">{passwordError}</div>
+                                    </div>
+
+                                    <div className="row mb-4">
+                                        <div className="col d-flex">
+                                            <div className="form-check">
+                                                <input 
+                                                    type="checkbox" 
+                                                    id="rememberMe" 
+                                                    className="form-check-input" 
+                                                />
+                                                <label className="form-check-label" htmlFor="rememberMe" style={{fontSize:'12px'}}> Remember me </label>
+                                            </div>
+                                        </div>
+                                        <div className="col">
+                                            <a href="#!" style={{fontSize:'12px'}}>Forgot password?</a>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button 
+                                            type="submit" 
+                                            className="btn btn-primary btn-block  px-5 shadow"
+                                        >
+                                            Sign in
+                                        </button>
+                                        {(usernameError || passwordError) && <div className="text-danger">Please fill in all required fields.</div>}
+                                    </div>
+                                </form>
                             </div>
-                            <div>
-                                <button 
-                                    type="submit" 
-                                    className="btn btn-primary btn-block mb-4 px-5 shadow"
-                                >
-                                    Sign in
-                                </button>
-                                {(usernameError || passwordError) && <div className="text-danger">Please fill in all required fields.</div>}
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Portfolio/Forms/Signup.jsx
+++ src/components/Portfolio/Forms/Signup.jsx
@@ -60,119 +60,123 @@ function Signup() {
     };
 
     return (
-        <div className="signup" id='signup'>
-            <div className="w3-container w3-content bg-white shadow">
-            <div className="w3-padding-16 border-dark">
-                    <h4 className="fw-light w3-margin-top"><b>Signup Form</b></h4>
-                    <form onSubmit={handleSubmit} autoComplete="on">
-                        <div className="row mb-4">
-                            <div className="col">
-                                <div className="form-outline">
-                                    <input 
-                                        type="text" 
-                                        id="firstName" 
-                                        className={`form-control shadow-sm border border-1 ${firstNameError ? 'is-invalid' : ''}`}
-                                        placeholder="First Name"
-                                        value={firstName}
-                                        onChange={(e) => setFirstName(e.target.value)}
-                                        autoComplete="given-name"
-                                    />
-                                    <div className="invalid-feedback">{firstNameError}</div>
+        <div className="row justify-content-center">
+            <div className="col-md-6 mx-auto">
+                <div className="signup" id='signup'>
+                    <div className="w3-container w3-content bg-white shadow ">
+                        <div className="w3-padding-16 border-dark px-2">
+                            <h4 className="fw-light w3-margin-top w3-margin-bottom"><b>Signup Form</b></h4>
+                            <form onSubmit={handleSubmit} autoComplete="on" >
+                                <div className="row my-4">
+                                    <div className="col">
+                                        <div className="form-outline">
+                                            <input 
+                                                type="text" 
+                                                id="firstName" 
+                                                className={`form-control shadow-sm border border-1 ${firstNameError ? 'is-invalid' : ''}`}
+                                                placeholder="First Name"
+                                                value={firstName}
+                                                onChange={(e) => setFirstName(e.target.value)}
+                                                autoComplete="given-name"
+                                            />
+                                            <div className="invalid-feedback">{firstNameError}</div>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="form-outline">
+                                            <input 
+                                                type="text" 
+                                                id="lastName" 
+                                                className={`form-control shadow-sm border border-1 ${lastNameError ? 'is-invalid' : ''}`}
+                                                placeholder="Last Name"
+                                                value={lastName}
+                                                onChange={(e) => setLastName(e.target.value)}
+                                                autoComplete="family-name"
+                                            />
+                                            <div className="invalid-feedback">{lastNameError}</div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col">
-                                <div className="form-outline">
+
+                                <div className="form-outline mb-4">
                                     <input 
-                                        type="text" 
-                                        id="lastName" 
-                                        className={`form-control shadow-sm border border-1 ${lastNameError ? 'is-invalid' : ''}`}
-                                        placeholder="Last Name"
-                                        value={lastName}
-                                        onChange={(e) => setLastName(e.target.value)}
-                                        autoComplete="family-name"
+                                        type="email" 
+                                        id="email" 
+                                        className={`form-control shadow-sm border border-1 ${emailError ? 'is-invalid' : ''}`}
+                                        placeholder="Email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        autoComplete="email"
                                     />
-                                    <div className="invalid-feedback">{lastNameError}</div>
+                                    <div className="invalid-feedback">{emailError}</div>
                                 </div>
-                            </div>
+
+                                <div className="form-outline mb-4">
+                                    <input 
+                                        type="password" 
+                                        id="anewpassword" 
+                                        className={`form-control shadow-sm border border-1 ${passwordError ? 'is-invalid' : ''}`}
+                                        placeholder="Password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        autoComplete="new-password"
+                                    />
+                                    <div className="invalid-feedback">{passwordError}</div>
+                                </div>
+
+                                <div className="form-check d-flex justify-content-center mb-4">
+                                    <input 
+                                        className="form-check-input me-2" 
+                                        type="checkbox" 
+                                        id="newsletterCheck"  
+                                        checked={newsletterChecked}
+                                        onChange={(e) => setNewsletterChecked(e.target.checked)}
+                                    />
+                                    <label className="form-check-label" htmlFor="newsletterCheck">
+                                        Subscribe to our newsletter
+                                    </label>
+                                </div>
+
+                                <button 
+                                    type="submit" 
+                                    className="btn btn-primary btn-block mb-4 px-5 shadow"
+                                >
+                                    Sign up
+                                </button>
+
+                                <div className="text-center w3-margin-bottom">
+                                    <p>or sign up with:</p>
+                                    <button 
+                                        type="button" 
+                                        className="btn btn-secondary btn-floating mx-1 shadow"
+                                    >
+                                        <i className="fab fa-facebook-f"></i>
+                                    </button>
+
+                                    <button 
+                                        type="button" 
+                                        className="btn btn-secondary btn-floating mx-1 shadow"
+                                    >
+                                        <i className="fab fa-google"></i>
+                                    </button>
+
+                                    <button  
+                                        type="button" 
+                                        className="btn btn-secondary btn-floating mx-1 shadow"
+                                    >
+                                        <i className="fab fa-twitter"></i>
+                                    </button>
+
+                                    <button 
+                                        type="button" 
+                                        className="btn btn-secondary btn-floating mx-1 shadow"
+                                    >
+                                        <i className="fab fa-github"></i>
+                                    </button>
+                                </div>
+                            </form>
                         </div>
-
-                        <div className="form-outline mb-4">
-                            <input 
-                                type="email" 
-                                id="email" 
-                                className={`form-control shadow-sm border border-1 ${emailError ? 'is-invalid' : ''}`}
-                                placeholder="Email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                autoComplete="email"
-                            />
-                            <div className="invalid-feedback">{emailError}</div>
-                        </div>
-
-                        <div className="form-outline mb-4">
-                            <input 
-                                type="password" 
-                                id="anewpassword" 
-                                className={`form-control shadow-sm border border-1 ${passwordError ? 'is-invalid' : ''}`}
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                autoComplete="new-password"
-                            />
-                            <div className="invalid-feedback">{passwordError}</div>
-                        </div>
-
-                        <div className="form-check d-flex justify-content-center mb-4">
-                            <input 
-                                className="form-check-input me-2" 
-                                type="checkbox" 
-                                id="newsletterCheck"  
-                                checked={newsletterChecked}
-                                onChange={(e) => setNewsletterChecked(e.target.checked)}
-                            />
-                            <label className="form-check-label" htmlFor="newsletterCheck">
-                                Subscribe to our newsletter
-                            </label>
-                        </div>
-
-                        <button 
-                            type="submit" 
-                            className="btn btn-primary btn-block mb-4 px-5 shadow"
-                        >
-                            Sign up
-                        </button>
-
-                        <div className="text-center w3-margin-bottom">
-                            <p>or sign up with:</p>
-                            <button 
-                                type="button" 
-                                className="btn btn-secondary btn-floating mx-1 shadow"
-                            >
-                                <i className="fab fa-facebook-f"></i>
-                            </button>
-
-                            <button 
-                                type="button" 
-                                className="btn btn-secondary btn-floating mx-1 shadow"
-                            >
-                                <i className="fab fa-google"></i>
-                            </button>
-
-                            <button  
-                                type="button" 
-                                className="btn btn-secondary btn-floating mx-1 shadow"
-                            >
-                                <i className="fab fa-twitter"></i>
-                            </button>
-
-                            <button 
-                                type="button" 
-                                className="btn btn-secondary btn-floating mx-1 shadow"
-                            >
-                                <i className="fab fa-github"></i>
-                            </button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
