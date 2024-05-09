75ada9f314450eb8a5448308b8112bbbc033dc19
Carol Wargo <Carolwargo@gmail.com>
2024-05-09 11:51:06 -0400

    added tailwind

--- src/components/NavFixed.jsx
+++ src/components/NavFixed.jsx
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+
 import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -22,39 +23,34 @@ function BasicExample() {
     className="home"
     
     >
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="bg-body-tertiary shadow-sm fixed-top" >
           
-      <Container>
-        <Link to='/' className="nav-link">
-        <Navbar.Brand><b>CW</b>HOME</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" >
-          <FontAwesomeIcon icon={faBars} color="black" />
+      <Container fluid>
+      <Navbar.Brand as={Link} to="/">
+            <b>CW</b>HOME
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <FontAwesomeIcon icon={faBars} color="black" />
           </Navbar.Toggle>
-      
-        <Navbar.Collapse id='navbar-collapse'>
-          <Nav className="me-auto">
-            <Link to="/about#about" className="nav-link">ABOUT</Link>
-            <Link to="/resume#resume" className="nav-link">RESUME</Link>
-            <Link to="/page-samples#page-samples" className="nav-link">PAGE SAMPLES</Link>
-            <NavDropdown title="MY WORK" id="basic-nav-dropdown" className="me-auto bg-white ">
-            <div className='w3-padding-small bg-white' style={{border:'3px solid', borderRadius:'10px', zIndex:'-5'}}>
-     
-              <Link to="/portfolio#portfolio" className="nav-link">PORTFOLIO</Link>
-      
-          
-              <Link to="/forms#forms" className="nav-link">
-              FORMS
-              </Link>
-             <Link to="/server#server" className="nav-link">UTILITY APPS</Link>
-             <Link to="/graphics#graphics" className="nav-link">GRAPHICS</Link>
-           
-           
-            </div>
-            </NavDropdown>
-           
-            <Link to="/contact#contact" className="nav-link" >CONTACT</Link>
-          </Nav>
+          <Navbar.Collapse id="navbar-collapse">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/about#about">ABOUT</Nav.Link>
+              <Nav.Link as={Link} to="/resume#resume">RESUME</Nav.Link>
+              <Nav.Link as={Link} to="/page-samples#page-samples">PAGE SAMPLES</Nav.Link>
+              <NavDropdown title="MY WORK" id="basic-nav-dropdown">
+                <div style={{ border: '3px solid', borderRadius: '10px' }}>
+                  <NavDropdown.Item as={Link} to="/portfolio#web-components">COMPONENT SAMPLES</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/forms#forms">FORM SAMPLES</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/page-samples">SITE SAMPLES</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/server#server">UTILITY APPS</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/graphics#graphics">COMING SOON! <br />GRAPHIC ELEMENTS</NavDropdown.Item>
+                </div>
+              </NavDropdown>
+            </Nav>
+            {/* Move the CONTACT link to the right margin */}
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/contact#contact">CONTACT</Nav.Link>
+            </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
