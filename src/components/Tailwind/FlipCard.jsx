f6cbd8f8a33d5850bbb77ec33119d8dfde53b4a1
Carol Wargo <Carolwargo@gmail.com>
2024-05-10 10:18:45 -0400

    update

--- src/components/Tailwind/FlipCard.jsx
+++ src/components/Tailwind/FlipCard.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import GirlCamera from '../../assets/images/GirlCamera.png'
-
+import '../../tailwind.css';
 
 const FlipCard = () => {
 
     return (
         <div className='tw-flex tw-items-center tw-justify-center tw-min-h-screen tw-bg-slate-100'>
-            <div className='tw-group tw-h-96 tw-w-96 [perspective:1000px]'>
-            <div className='tw-relative tw-w-full tw-h-full tw-rounded-xl tw-shadow-xl tw-transition-all tw-duration-500 [transform-style:preserve-3d] tw-group-hover:[transform:rotateY(180deg)]'>
+            <div className='tw-group tw-h-96 tw-w-96 tw-[perspective:1000px]'>
+            <div className='tw-relative tw-w-full tw-h-full tw-rounded-xl tw-shadow-xl tw-transition-all tw-duration-500 tw-[transform-style:preserve-3d] tw-group-hover:[transform:rotateY(180deg)]'>
             <div className='tw-inset-0 tw-absolute'>
     <img  className='tw-shadow-xl tw-h-full tw-w-full tw-object-cover tw-rounded-xl tw-shadow-black/80' src={GirlCamera} alt='girl with camera'></img>
     </div>
     
-    <div className='tw-absolute tw-inset-0 tw-w-full tw-h-full tw-px-12 tw-text-center tw-rounded-xl tw-bg-black/80 tw-text-slate-200 [transform:rotateY(180)] [back visibility:hidden'>
+    <div className='tw-absolute tw-inset-0 tw-w-full tw-h-full tw-px-12 tw-text-center tw-rounded-xl tw-bg-black/80 tw-text-slate-200 tw-[transform:rotateY(180)] tw-[back visibility:hidden'>
    <div className='tw-min-h-full tw-justify-center tw-items-center tw-flex-col tw-flex'>
 <h1 className='tw-font-bold tw-text-3xl'>
   Joanna Doe
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: 'tw-',
-  content: [
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx',
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'
+
   ],
+
   theme: {
-    extend: {},
+    
+    extend: {
+      backgroundImage: {
+        page: "url('./img/background.png')",
+      },
+      fontFamily: {
+  sans: ['dmserif', 'DM Serif', 'serif'],
+  com: ['conforta', 'Conforta', 'sans-serif']
+},
+
+    },
   },
   plugins: [],
-}
+};
 
